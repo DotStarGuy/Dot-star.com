15a3d0756b72d1f9bae7c80112de3a4a2eb80948
Jakub Flaczynski <jakubflaczynski@gmail.com>
2023-03-21 14:27:19 +0100

    finished product

--- js/main.js
+++ js/main.js
@@ -1,125 +1,194 @@
-var inputField = document.getElementById("input");
-var response = document.getElementById("response");
-var a = document.createElement("a");
-inputField.focus();
-inputField.select();
-
-setInterval(function () {
-  if (document.activeElement !== inputField) {
-    inputField.focus();
-    inputField.select();
-  }
-}, 100);
-inputField.onkeydown = function (event) {
-  if (event.keyCode == 13) {
-    var userInput = document.getElementById("input").value.toLowerCase();
-    // create span && send the command that was used
-    var span = document.createElement("span");
-    var br = document.createElement("br");
-    var text = document.createTextNode("webuser@.★:~$ " + userInput);
-    span.appendChild(text);
-    span.appendChild(br);
-    // commands
-    switch (userInput) {
-      case "help":
-        var text = document.createTextNode(`
-Available commands:
-
-help          Displays this message.                                  discord         Links my Discord server.
-clear         Clears the terminal screen.                           reload          Reloads the page.
-whois        Displays some information about me.           pricing         General information about how much, and how i charge.
-blog          Redirects you to my blog.                              exit              Closes the current tab.`);
-        break;
-      case "clear":
-        var spans = document.getElementsByTagName("span");
-        var links = document.getElementsByTagName("a");
-        inputField.value = "";
-        while (links.length > 0) {
-          links[0].parentNode.removeChild(links[0]);
-        }
-        while (spans.length > -1) {
-          spans[0].parentNode.removeChild(spans[0]);
-        }
-        break;
-      case "whois":
-        var text = document.createTextNode(`
-Hi, thank you for visiting my site. My name is .★ (Dot Star) and I'm a 19 year old software development student based in the Netherlands.
-I specialise in web development but I can also work with Java, Python and Javascript.
-I also have some experience with Discord.js and Unity.
-                
-As for my hobbies, I really enjoy coding and working out, I also play League of legends… like a lot(I seriously need to stop).
-I used to play keyboard but quit because it started to become very frustrating.
-        
-I enjoy the following music genres: Indie, LoFi, Breakcore, classical and also hardstyle from time to time.
-Sadly I don’t create any music of my own at this moment but I would love to try sometime in the future.
-        
-And well, that's about all I can share because I like my privacy and staying anonymous as much as possible.
-Thank you for taking the time to read the little about me and if you’re interested in hiring me you can message me on Discord .★#0916.
-`);
-        break;
-      case "blog":
-        var text = document.createTextNode(`
-Heh, so about the blog thing...
-It isn't done yet.
-It's something I will create in the future... maybe... hopefully (probably not).`);
-        break;
-      case "exit":
-        window.close();
-        break;
-      case "discord":
-        var div = document.createElement("div");
-        div.setAttribute("class", "discord");
-        // discord
-        a.setAttribute("href", "https://discord.gg/PW7t6NBs46");
-        a.textContent = `
-Here's a link to my Discord servers.
-
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⡟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢹⣿⣿⣿
-⣿⣿⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⣿
-⣿⣿⣿⡇⠄⠄⠄⢠⣴⣾⣵⣶⣶⣾⣿⣦⡄⠄⠄⠄⢸⣿⣿⣿
-⣿⣿⣿⡇⠄⠄⢀⣾⣿⣿⢿⣿⣿⣿⣿⣿⣿⡄⠄⠄⢸⣿⣿⣿
-⣿⣿⣿⡇⠄⠄⢸⣿⣿⣧⣀⣼⣿⣄⣠⣿⣿⣿⠄⠄⢸⣿⣿⣿
-⣿⣿⣿⡇⠄⠄⠘⠻⢷⡯⠛⠛⠛⠛⢫⣿⠟⠛⠄⠄⢸⣿⣿⣿
-⣿⣿⣿⡇⠄  Add me on diccord! ⢸⣿⣿⣿
-⣿⣿⣿⣧⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣆⣸⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿`;
-
-        div.appendChild(a);
-        response.appendChild(div);
-        break;
-      case "reload":
-        location.reload();
-        break;
-      case "pricing":
-        var text = document.createTextNode(`
-Okay, let's talk money.
-The price of a script/program really depends on the size of the project.
-Small size script €20
-Medium size script €50
-Big scripts €70
-Those prices are an estimate.
-I would need to know what you want exactly in order to give you an accurate price.
-For full either GUI or CLI programs contact me on Discord and we can talk about it.
-
-I do all transactions through paypal for alternative payment methods contact me on Discord.`);
-        break;
-      case "exit":
-        window.close();
-        break;
-      default:
-        var text = document.createTextNode(
-          "Unknown command, type 'help' for the list of commands"
-        );
+function _0x5f39(_0x1c382e, _0x2112a2) {
+  var _0x1800e1 = _0x1800();
+  return (
+    (_0x5f39 = function (_0x5f39c2, _0x11523c) {
+      _0x5f39c2 = _0x5f39c2 - 0x11d;
+      var _0x430375 = _0x1800e1[_0x5f39c2];
+      return _0x430375;
+    }),
+    _0x5f39(_0x1c382e, _0x2112a2)
+  );
+}
+var _0xa0462f = _0x5f39;
+(function (_0x180790, _0x473a4a) {
+  var _0x512cf1 = _0x5f39,
+    _0x21502e = _0x180790();
+  while (!![]) {
+    try {
+      var _0x4134b6 =
+        (parseInt(_0x512cf1(0x14f)) / 0x1) *
+          (parseInt(_0x512cf1(0x121)) / 0x2) +
+        -parseInt(_0x512cf1(0x12e)) / 0x3 +
+        (-parseInt(_0x512cf1(0x14e)) / 0x4) *
+          (parseInt(_0x512cf1(0x156)) / 0x5) +
+        (parseInt(_0x512cf1(0x12d)) / 0x6) *
+          (parseInt(_0x512cf1(0x153)) / 0x7) +
+        (-parseInt(_0x512cf1(0x140)) / 0x8) *
+          (-parseInt(_0x512cf1(0x152)) / 0x9) +
+        (-parseInt(_0x512cf1(0x144)) / 0xa) *
+          (-parseInt(_0x512cf1(0x145)) / 0xb) +
+        (-parseInt(_0x512cf1(0x12c)) / 0xc) *
+          (parseInt(_0x512cf1(0x13c)) / 0xd);
+      if (_0x4134b6 === _0x473a4a) break;
+      else _0x21502e["push"](_0x21502e["shift"]());
+    } catch (_0x5010e6) {
+      _0x21502e["push"](_0x21502e["shift"]());
     }
-    // clear the input field
-    inputField.value = "";
-
-    // appending the text
-    span.appendChild(text);
-    response.appendChild(span);
-    // keep the input field in view
-    inputField.scrollIntoView();
   }
-};
+})(_0x1800, 0x26bb6);
+function _0x1800() {
+  var _0x5c2623 = [
+    "cd\x20dev",
+    "pricing",
+    "discord",
+    "https://discord.gg/PW7t6NBs46",
+    "840IDHdDH",
+    "10WeTuBH",
+    "cd\x20bin",
+    "div",
+    "572553eiIqKU",
+    "111363iVATnt",
+    "\x0aHeh,\x20so\x20about\x20the\x20blog\x20thing...\x0aIt\x20isn\x27t\x20done\x20yet.\x0aIt\x27s\x20something\x20I\x20will\x20create\x20in\x20the\x20future...\x20maybe...\x20hopefully\x20(probably\x20not).",
+    "activeElement",
+    "480fBsTVn",
+    "setAttribute",
+    "getElementById",
+    "parentNode",
+    "input",
+    "60500bRmiQq",
+    "\x0aAvailable\x20commands:\x0a\x0ahelp\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20Displays\x20this\x20message.\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20discord\x20\x20\x20\x20\x20\x20\x20\x20\x20Links\x20my\x20Discord\x20server.\x0aclear\x20\x20\x20\x20\x20\x20\x20\x20\x20Clears\x20the\x20terminal\x20screen.\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20reload\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20Reloads\x20the\x20page.\x0awhois\x20\x20\x20\x20\x20\x20\x20\x20Displays\x20some\x20information\x20about\x20me.\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20pricing\x20\x20\x20\x20\x20\x20\x20\x20\x20General\x20information\x20about\x20how\x20much,\x20and\x20how\x20i\x20charge.\x0ablog\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20Redirects\x20you\x20to\x20my\x20blog.\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20exit\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20Closes\x20the\x20current\x20tab.",
+    "createTextNode",
+    "close",
+    "help",
+    "scrollIntoView",
+    "exit",
+    "appendChild",
+    "cd\x20home",
+    "\x0abin\x20\x20\x20dev\x20\x20\x20etc\x20\x20\x20usr\x20\x20\x20home\x0a\x0alib\x20\x20\x20sbin\x20\x20tmp\x20\x20\x20var\x20\x20\x20personal\x0a\x20\x20\x20\x20\x20\x20\x20\x20",
+    "cd\x20tmp",
+    "4481820OVkCbG",
+    "102ADlOGU",
+    "930531POIzPp",
+    "cd\x20etc",
+    "getElementsByTagName",
+    "reload",
+    "cd\x20lib",
+    "\x0aHi,\x20thank\x20you\x20for\x20visiting\x20my\x20site.\x20My\x20name\x20is\x20.★\x20(Dot\x20Star)\x20and\x20I\x27m\x20a\x2019\x20year\x20old\x20software\x20development\x20student\x20based\x20in\x20the\x20Netherlands.\x0aI\x20specialise\x20in\x20web\x20development\x20but\x20I\x20can\x20also\x20work\x20with\x20Java,\x20Python\x20and\x20Javascript.\x0aI\x20also\x20have\x20some\x20experience\x20with\x20Discord.js\x20and\x20Unity.\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0aAs\x20for\x20my\x20hobbies,\x20I\x20really\x20enjoy\x20coding\x20and\x20working\x20out,\x20I\x20also\x20play\x20League\x20of\x20legends…\x20like\x20a\x20lot(I\x20seriously\x20need\x20to\x20stop).\x0aI\x20used\x20to\x20play\x20keyboard\x20but\x20quit\x20because\x20it\x20started\x20to\x20become\x20very\x20frustrating.\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x0aI\x20enjoy\x20the\x20following\x20music\x20genres:\x20Indie,\x20LoFi,\x20Breakcore,\x20classical\x20and\x20also\x20hardstyle\x20from\x20time\x20to\x20time.\x0aSadly\x20I\x20don’t\x20create\x20any\x20music\x20of\x20my\x20own\x20at\x20this\x20moment\x20but\x20I\x20would\x20love\x20to\x20try\x20sometime\x20in\x20the\x20future.\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x0aAnd\x20well,\x20that\x27s\x20about\x20all\x20I\x20can\x20share\x20because\x20I\x20like\x20my\x20privacy\x20and\x20staying\x20anonymous\x20as\x20much\x20as\x20possible.\x0aThank\x20you\x20for\x20taking\x20the\x20time\x20to\x20read\x20the\x20little\x20about\x20me\x20and\x20if\x20you’re\x20interested\x20in\x20hiring\x20me\x20you\x20can\x20message\x20me\x20on\x20Discord\x20.★#0916.\x0a",
+    "\x0aInsufficient\x20privileges\x20to\x20access\x20this\x20directory,\x20are\x20you\x20logged\x20into\x20the\x20correct\x20account?\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
+    "class",
+    "cd\x20var",
+    "focus",
+    "keyCode",
+    "span",
+    "\x0aHere\x27s\x20a\x20link\x20to\x20my\x20Discord\x20servers.\x0a\x0a⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\x0a⣿⣿⣿⡟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢹⣿⣿⣿\x0a⣿⣿⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⣿\x0a⣿⣿⣿⡇⠄⠄⠄⢠⣴⣾⣵⣶⣶⣾⣿⣦⡄⠄⠄⠄⢸⣿⣿⣿\x0a⣿⣿⣿⡇⠄⠄⢀⣾⣿⣿⢿⣿⣿⣿⣿⣿⣿⡄⠄⠄⢸⣿⣿⣿\x0a⣿⣿⣿⡇⠄⠄⢸⣿⣿⣧⣀⣼⣿⣄⣠⣿⣿⣿⠄⠄⢸⣿⣿⣿\x0a⣿⣿⣿⡇⠄⠄⠘⠻⢷⡯⠛⠛⠛⠛⢫⣿⠟⠛⠄⠄⢸⣿⣿⣿\x0a⣿⣿⣿⡇⠄\x20\x20Add\x20me\x20on\x20diccord!\x20⢸⣿⣿⣿\x0a⣿⣿⣿⣧⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⣿\x0a⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣆⣸⣿⣿⣿\x0a⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
+    "createElement",
+    "13aMXrAK",
+    "textContent",
+    "webuser@.★:~$\x20",
+    "select",
+    "32VBamQn",
+    "whois",
+    "blog",
+    "href",
+    "3470iVrUVf",
+    "1111PsrriG",
+    "onkeydown",
+    "length",
+    "removeChild",
+    "value",
+  ];
+  _0x1800 = function () {
+    return _0x5c2623;
+  };
+  return _0x1800();
+}
+var inputField = document[_0xa0462f(0x11e)](_0xa0462f(0x120)),
+  response = document[_0xa0462f(0x11e)]("response"),
+  a = document[_0xa0462f(0x13b)]("a");
+inputField[_0xa0462f(0x137)](),
+  inputField["select"](),
+  setInterval(function () {
+    var _0x2a12d7 = _0xa0462f;
+    document[_0x2a12d7(0x155)] !== inputField &&
+      (inputField[_0x2a12d7(0x137)](), inputField[_0x2a12d7(0x13f)]());
+  }, 0x64),
+  (inputField[_0xa0462f(0x146)] = function (_0x24c3f1) {
+    var _0x5c666f = _0xa0462f;
+    if (_0x24c3f1[_0x5c666f(0x138)] == 0xd) {
+      var _0x4ddd02 = document[_0x5c666f(0x11e)](_0x5c666f(0x120))[
+          _0x5c666f(0x149)
+        ]["toLowerCase"](),
+        _0x57e24d = document[_0x5c666f(0x13b)](_0x5c666f(0x139)),
+        _0x334211 = document[_0x5c666f(0x13b)]("br"),
+        _0x2c26ea = document[_0x5c666f(0x123)](_0x5c666f(0x13e) + _0x4ddd02);
+      _0x57e24d[_0x5c666f(0x128)](_0x2c26ea),
+        _0x57e24d[_0x5c666f(0x128)](_0x334211);
+      switch (_0x4ddd02) {
+        case _0x5c666f(0x125):
+          var _0x2c26ea = document[_0x5c666f(0x123)](_0x5c666f(0x122));
+          break;
+        case "clear":
+          var _0xfd3018 = document[_0x5c666f(0x130)]("span"),
+            _0x285841 = document[_0x5c666f(0x130)]("a");
+          inputField[_0x5c666f(0x149)] = "";
+          while (_0x285841[_0x5c666f(0x147)] > 0x0) {
+            _0x285841[0x0]["parentNode"]["removeChild"](_0x285841[0x0]);
+          }
+          while (_0xfd3018[_0x5c666f(0x147)] > -0x1) {
+            _0xfd3018[0x0][_0x5c666f(0x11f)][_0x5c666f(0x148)](_0xfd3018[0x0]);
+          }
+          break;
+        case _0x5c666f(0x141):
+          var _0x2c26ea = document[_0x5c666f(0x123)](_0x5c666f(0x133));
+          break;
+        case _0x5c666f(0x142):
+          var _0x2c26ea = document["createTextNode"](_0x5c666f(0x154));
+          break;
+        case _0x5c666f(0x127):
+          window[_0x5c666f(0x124)]();
+          break;
+        case _0x5c666f(0x14c):
+          var _0x370177 = document[_0x5c666f(0x13b)](_0x5c666f(0x151));
+          _0x370177[_0x5c666f(0x11d)](_0x5c666f(0x135), _0x5c666f(0x14c)),
+            a[_0x5c666f(0x11d)](_0x5c666f(0x143), _0x5c666f(0x14d)),
+            (a[_0x5c666f(0x13d)] = _0x5c666f(0x13a)),
+            _0x370177[_0x5c666f(0x128)](a),
+            response[_0x5c666f(0x128)](_0x370177);
+          break;
+        case _0x5c666f(0x131):
+          location[_0x5c666f(0x131)]();
+          break;
+        case _0x5c666f(0x14b):
+          var _0x2c26ea = document["createTextNode"](
+            "\x0aOkay,\x20let\x27s\x20talk\x20money.\x0aThe\x20price\x20of\x20a\x20script/program\x20really\x20depends\x20on\x20the\x20size\x20of\x20the\x20project.\x0aSmall\x20size\x20script\x20€20\x0aMedium\x20size\x20script\x20€50\x0aBig\x20scripts\x20€70\x0aThose\x20prices\x20are\x20an\x20estimate.\x0aI\x20would\x20need\x20to\x20know\x20what\x20you\x20want\x20exactly\x20in\x20order\x20to\x20give\x20you\x20an\x20accurate\x20price.\x0aFor\x20full\x20either\x20GUI\x20or\x20CLI\x20programs\x20contact\x20me\x20on\x20Discord\x20and\x20we\x20can\x20talk\x20about\x20it.\x0a\x0aI\x20do\x20all\x20transactions\x20through\x20paypal\x20for\x20alternative\x20payment\x20methods\x20contact\x20me\x20on\x20Discord."
+          );
+          break;
+        case _0x5c666f(0x127):
+          window[_0x5c666f(0x124)]();
+          break;
+        case "ls":
+          var _0x2c26ea = document[_0x5c666f(0x123)](_0x5c666f(0x12a));
+          break;
+        case _0x5c666f(0x150):
+        case _0x5c666f(0x14a):
+        case _0x5c666f(0x12f):
+        case "cd\x20usr":
+        case _0x5c666f(0x129):
+        case _0x5c666f(0x132):
+        case "cd\x20sbin":
+        case _0x5c666f(0x12b):
+        case _0x5c666f(0x136):
+        case "cd\x20personal":
+          var _0x2c26ea = document[_0x5c666f(0x123)](_0x5c666f(0x134));
+          break;
+        default:
+          var _0x2c26ea = document[_0x5c666f(0x123)](
+            "Unknown\x20command,\x20type\x20\x27help\x27\x20for\x20the\x20list\x20of\x20commands"
+          );
+      }
+      (inputField[_0x5c666f(0x149)] = ""),
+        _0x57e24d[_0x5c666f(0x128)](_0x2c26ea),
+        response[_0x5c666f(0x128)](_0x57e24d),
+        inputField[_0x5c666f(0x126)]();
+    }
+  });
